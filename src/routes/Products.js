b8632b4ff45484497a7f9f6cac391e7475d7ba2b
ben awad <benawad97@gmail.com>
2018-02-07 12:48:37 -0600

    fetch products

--- src/routes/Products.js
+++ src/routes/Products.js
@@ -1,9 +1,47 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Image, Text, View, Button, FlatList } from 'react-native';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
-export default ({ history }) => (
-  <View>
-    <Text style={{ marginTop: 50 }}>this is the products page</Text>
-    <Button title="Create Product" onPress={() => history.push('/new-product')} />
-  </View>
-);
+const Products = ({ data: { products }, loading, history }) => {
+  if (loading || !products) {
+    return null;
+  }
+
+  const productsWithKey = products.map(p => ({
+    ...p,
+    key: p.id,
+  }));
+
+  console.log(productsWithKey[0]);
+
+  return (
+    <View>
+      <Text style={{ marginTop: 50 }}>this is the products page</Text>
+      <Button title="Create Product" onPress={() => history.push('/new-product')} />
+      <FlatList
+        data={productsWithKey}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.name}</Text>
+            <Text>{item.price}</Text>
+            <Image source={{ uri: item.pictureUrl }} />
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+const productsQuery = gql`
+  {
+    products {
+      id
+      price
+      pictureUrl
+      name
+    }
+  }
+`;
+
+export default graphql(productsQuery)(Products);
