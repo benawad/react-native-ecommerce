d59f0e52dfb07bf825205807213b780be4281334
ben awad <benawad97@gmail.com>
2018-02-08 10:23:30 -0600

    forgot to commit this too :)

--- src/routes/Products.js
+++ src/routes/Products.js
@@ -1,7 +1,32 @@
 import React from 'react';
-import { Image, Text, View, Button, FlatList } from 'react-native';
+import { Image, Text, View, Button, FlatList, StyleSheet } from 'react-native';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+
+const styles = StyleSheet.create({
+  images: {
+    height: 100,
+    width: 100,
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    margin: 10,
+  },
+  right: {
+    marginLeft: 10,
+    marginRight: 30,
+    flex: 1,
+    display: 'flex',
+    alignItems: 'flex-end',
+  },
+  name: {
+    fontSize: 40,
+  },
+  price: {
+    fontSize: 30,
+  },
+});
 
 const Products = ({ data: { products }, loading, history }) => {
   if (loading || !products) {
@@ -17,15 +42,19 @@ const Products = ({ data: { products }, loading, history }) => {
 
   return (
     <View>
-      <Text style={{ marginTop: 50 }}>this is the products page</Text>
       <Button title="Create Product" onPress={() => history.push('/new-product')} />
       <FlatList
         data={productsWithKey}
         renderItem={({ item }) => (
-          <View>
-            <Text>{item.name}</Text>
-            <Text>{item.price}</Text>
-            <Image source={{ uri: item.pictureUrl }} />
+          <View style={styles.row}>
+            <Image
+              style={styles.images}
+              source={{ uri: `http://localhost:4000/${item.pictureUrl}` }}
+            />
+            <View style={styles.right}>
+              <Text style={styles.name}>{item.name}</Text>
+              <Text style={styles.price}>{`$${item.price}`}</Text>
+            </View>
           </View>
         )}
       />
